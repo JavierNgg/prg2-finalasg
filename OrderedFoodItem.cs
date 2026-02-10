94ccca99b401ae4a7a98d724d8e36b800754e9d1
JavierNgg <s10272509@connect.np.edu.sg>
2026-02-10 22:06:18 +0800

    Add files via upload

---
+++ OrderedFoodItem.cs
@@ -0,0 +1,34 @@
+﻿//==========================================================
+// Student Number : S10272509G
+// Student Name : Javier Ng Zhe Wei
+// Partner Name : Axel Tee Yu Le
+//==========================================================
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10271009C_PRG2Assignment
+{
+    class OrderedFoodItem : FoodItem
+    {
+        public int qtyOrdered {  get; set; }
+        public double subTotal { get; set; }
+
+        public OrderedFoodItem() { }
+        public OrderedFoodItem(FoodItem foodItem, int qtyOrdered) : base(foodItem.itemName, foodItem.itemDesc, foodItem.itemPrice)
+        {
+            this.qtyOrdered = qtyOrdered;
+            this.customise = customise;
+            this.subTotal = CalculateSubtotal(); ;
+        }
+
+        public double CalculateSubtotal()
+        {
+            this.subTotal = itemPrice * qtyOrdered;
+            return subTotal;
+        }
+    }
+}
---
+++ SpecialOffer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//==========================================================
+// Student Number : S10271009C
+// Student Name : Axel Tee Yu Le
+// Partner Name : Javier Ng Zhe Wei
+//==========================================================
+
+namespace S10271009C_PRG2Assignment
+{
+    class SpecialOffer
+    {
+        public string offerCode { get; set; }
+        public string offerDesc { get; set; }
+        public double discount { get; set; }
+        public SpecialOffer() { }
+        public SpecialOffer(string offerCode, string offerDesc, double discount) 
+        {
+            this.offerCode = offerCode;
+            this.offerDesc = offerDesc;
+            this.discount = discount;
+        }
+    }
+}
